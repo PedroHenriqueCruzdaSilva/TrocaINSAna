eba763195de9cd46337edf34a177625ab9328f33
PedroHenriqueCruzDaSilva <samuraigamer100000@gmail.com>
2023-04-14 19:10:42 -0300

    Home-1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 function App() {
   return (
     <div className="App">
@@ -5,7 +6,26 @@ function App() {
         <a href="/">Troca INSAna</a>
       </header>
       <main>
-        Lista Roupas
+        <h1>Em destaque</h1>
+        <div className="products">
+          {
+            data.products.map((product) => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} widht="250px" height="250px" />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p className="product-info_name">{product.name}</p>
+                  </a>
+                  <p className="product-info_desc">{product.description}</p>
+                  <p className="product-info_email">E-mail para troca abaixo:</p>
+                  <strong>{product.email}</strong>
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </main>
     </div>
   );
