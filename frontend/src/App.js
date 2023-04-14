2676d7bd8a90b9ac2e6b0dda5536067f1e47a3a8
PedroHenriqueCruzDaSilva <samuraigamer100000@gmail.com>
2023-04-14 17:03:37 -0300

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Troca INSAna</a>
+      </header>
+      <main>
+        Lista Roupas
+      </main>
+    </div>
+  );
+}
+
+export default App;
