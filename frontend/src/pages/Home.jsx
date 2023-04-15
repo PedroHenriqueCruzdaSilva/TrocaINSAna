81e0745270e474ca94ceaac6fb2a007bfccb65a3
PedroHenriqueCruzDaSilva <samuraigamer100000@gmail.com>
2023-04-14 21:07:52 -0300

    Reducer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, /* useState */ } from 'react';
 // import data from '../data';
 import axios from "axios";
 import { Link } from 'react-router-dom';
+import logger from 'use-reducer-logger';
+
+const reducer = (state, action)  => {
+    switch(action.type) {
+        case 'FETCH_REQUEST':
+            return {...state, loading: true}
+        case 'FETCH_SUCCESS':
+            return {...state, products: action.playload, loading: false}
+        case 'FETCH_FAIL':
+            return {...state, loading: false, error: action.playload}
+        default:
+            return state
+    }
+}
 
 export default function Home() {
-    const [products, setProducts] = useState([]);
+    const [{loading, error, products}, dispatch] = useReducer(logger(reducer), {
+        products: [],
+        loading: true,
+        error: false
+    })
+    // const [products, setProducts] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios.get('/api/products')
-            setProducts(result.data)
+            dispatch({type: 'FETCH_REQUEST'})
+            try {
+                const result = await axios.get('/api/products') 
+                dispatch({type: 'FETCH_SUCCESS', playload: result.data})
+            } catch (err) {
+                dispatch({type: 'FETCH_FAIL', playload: err.message})
+            }
+            // setProducts(result.data)
         }
         fetchData()
     }, [])
@@ -17,10 +42,14 @@ export default function Home() {
           <h1>Em destaque</h1>
             <div className="products">
                 {
-                products.map((product) => (
+                loading ? (
+                    <div> Loading. </div>
+               ) : error ? ( 
+                <div> {error} </div> 
+               ) : (products.map((product) => (
                     <div className="product" key={product.slug}>
                     <Link to={`/product/${product.slug}`}>
-                        <img src={product.image} alt={product.name} widht="250px" height="250px" />
+                        <img src={product.image} alt={product.name} widht="300px" height="300px" />
                     </Link>
                     <div className="product-info">
                         <Link to={`/product/${product.slug}`}>
@@ -32,7 +61,7 @@ export default function Home() {
                     </div>
                     </div>
                 ))
-                }
+                ) }
             </div>
           </>
     )
